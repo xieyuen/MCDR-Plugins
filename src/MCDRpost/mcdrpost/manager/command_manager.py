c39c0fb9d370cc259a67149a73427f31d0264121
xieyuen <xieyuenol@outlook.com>
2025-08-27 00:00:18 +0800

    feat: 适应不同 Minecraft 版本 (#3)
    * feat: 适应不同 Minecraft 版本
    
    * update demo
    
    * fix: error import
    
    * fix: cannot receive posts
    
    * feat: 给订单排序
    
    * fix: 恢复误删的代码
    
    * fix: 补全提示
    
    * 更新demo

--- src/MCDRpost/mcdrpost/manager/command_manager.py
+++ src/MCDRpost/mcdrpost/manager/command_manager.py
@@ -3,7 +3,7 @@ from typing import Literal as LiteralType, TYPE_CHECKING
 from mcdreforged import CommandSource, GreedyText, InfoCommandSource, Integer, Literal, PluginServerInterface, RAction, \
     RColor, RText, RTextList, RequirementNotMet, Text
 
-from mcdrpost.config.configuration import CommandPermission
+from mcdrpost.configuration import CommandPermission
 from mcdrpost.constants import END_LINE
 from mcdrpost.utils import tr
 from mcdrpost.utils.translation_tags import Tags
@@ -18,11 +18,13 @@ class CommandManager:
     def __init__(self, post_manager: "PostManager") -> None:
         self._post_manager: "PostManager" = post_manager
         self._server: PluginServerInterface = post_manager.server
+        self._data_manager = post_manager.data_manager
+        self._perm: CommandPermission = post_manager.config_manager.configuration.command_permission
+
         if self._post_manager.config_manager.configuration.allow_alias:
             self._prefixes: list[str] = post_manager.config_manager.configuration.command_prefixes
         else:
             self._prefixes = ["!!po"]
-        self._perm: CommandPermission = post_manager.config_manager.configuration.command_permission
 
     def register(self) -> None:
         """注册命令树
@@ -38,6 +40,7 @@ class CommandManager:
                 self.generate_command_node(prefix)
             )
 
+    # helper methods
     def output_help_message(self, source: CommandSource, prefix: str) -> None:
         """辅助函数：打印帮助信息"""
         msgs_on_helper = RText('')
@@ -101,7 +104,7 @@ class CommandManager:
 
     def output_post_list(self, src: InfoCommandSource) -> None:
         """辅助函数：输出玩家发送的订单列表"""
-        post_list = self._post_manager.order_manager.get_orders_by_sender(
+        post_list = self._data_manager.get_orders_by_sender(
             src.get_info().player
         )
 
@@ -126,7 +129,7 @@ class CommandManager:
 
     def output_receive_list(self, src: InfoCommandSource) -> None:
         """辅助函数：输出玩家待接收的邮件列表"""
-        receive_list = self._post_manager.order_manager.get_orders_by_receiver(
+        receive_list = self._data_manager.get_orders_by_receiver(
             src.get_info().player
         )
 
@@ -151,7 +154,7 @@ class CommandManager:
 
     def output_all_orders(self, src: InfoCommandSource) -> None:
         """辅助函数：输出所有订单列表"""
-        all_orders = self._post_manager.order_manager.get_orders()
+        all_orders = self._data_manager.get_orders()
 
         if not all_orders:
             src.reply(tr(Tags.no_orders))
@@ -170,6 +173,16 @@ class CommandManager:
             .format(tr(Tags.list_orders_title), msg)
         )
 
+    def receive(self, src: InfoCommandSource, order_id):
+        if self._post_manager.receive(src, order_id, 'receive'):
+            src.reply(tr(Tags.receive_success, order_id))
+    def cancel(self, src: InfoCommandSource, order_id):
+        if self._post_manager.receive(src, order_id, 'cancel'):
+            src.reply(tr(Tags.cancel_success, order_id))
+
+
+    # nodes
+
     def gen_post_node(self, node_name: str) -> Literal:
         return (
             Literal(node_name).
@@ -180,7 +193,7 @@ class CommandManager:
             runs(lambda src: src.reply(tr(Tags.no_input_receiver))).
             then(
                 Text('receiver').
-                suggests(self._post_manager.order_manager.get_players).
+                suggests(self._data_manager.get_players).
                 runs(lambda src, ctx: self._post_manager.post(src, ctx['receiver'])).
                 then(
                     GreedyText('comment').
@@ -212,15 +225,10 @@ class CommandManager:
                 suggests(
                     lambda src: [
                         str(i) for i in
-                        self._post_manager.order_manager.get_orderid_by_receiver(src.get_info().player)
+                        self._data_manager.get_orderid_by_receiver(src.get_info().player)
                     ]
                 ).
-                runs(
-                    lambda src, ctx: (
-                        self._post_manager.receive(src, ctx['orderid']),
-                        src.reply(tr(Tags.receive_success, ctx['orderid']))
-                    )
-                )
+                runs(lambda src, ctx: self.receive(src, ctx['orderid']))
             )
         )
 
@@ -247,15 +255,10 @@ class CommandManager:
                 suggests(
                     lambda src: [
                         str(i) for i in
-                        self._post_manager.order_manager.get_orderid_by_sender(src.get_info().player)
+                        self._data_manager.get_orderid_by_sender(src.get_info().player)
                     ]
                 ).
-                runs(
-                    lambda src, ctx: (
-                        self._post_manager.receive(src, ctx['orderid']),
-                        src.reply(tr(Tags.cancel_success, ctx['orderid']))
-                    )
-                )
+                runs(lambda src, ctx: self.cancel(src, ctx['orderid']))
             )
         )
 
@@ -267,7 +270,7 @@ class CommandManager:
                 Literal('players').
                 requires(lambda src: src.has_permission(self._perm.list_player)).
                 runs(lambda src: src.reply(
-                    tr(Tags.list_player_title) + str(self._post_manager.order_manager.get_players())
+                    tr(Tags.list_player_title) + str(self._data_manager.get_players())
                 ))
             ).
             then(
@@ -304,7 +307,7 @@ class CommandManager:
                 runs(lambda src: src.reply(tr(Tags.command_incomplete))).
                 then(
                     Text('player_id').
-                    runs(lambda src, ctx: self._post_manager.order_manager.add_player(ctx['player_id']))
+                    runs(lambda src, ctx: self._data_manager.add_player(ctx['player_id']))
                 )
             ).
             then(
@@ -312,8 +315,8 @@ class CommandManager:
                 runs(lambda src: src.reply(tr(Tags.command_incomplete))).
                 then(
                     Text('player_id').
-                    suggests(self._post_manager.order_manager.get_players).
-                    runs(lambda src, ctx: self._post_manager.order_manager.remove_player(ctx['player_id']))
+                    suggests(self._data_manager.get_players).
+                    runs(lambda src, ctx: self._data_manager.remove_player(ctx['player_id']))
                 )
             )
         )
@@ -338,7 +341,7 @@ class CommandManager:
             ).
             then(
                 Literal('orders').
-                runs(getattr(self._post_manager.order_manager, t))
+                runs(getattr(self._data_manager, t))
             )
         )
 
@@ -355,21 +358,17 @@ class CommandManager:
             requires(lambda src: src.has_permission(self._perm.root)).
             on_error(RequirementNotMet, lambda src: src.reply(tr(Tags.no_permission)), handled=True).
             runs(lambda src: self.output_help_message(src, prefix)).
-            then(self.gen_post_node('p')).
-            then(self.gen_post_node('post')).
-            then(self.gen_post_list_node('pl')).
-            then(self.gen_post_list_node('post_list')).
-            then(self.gen_receive_node('r')).
-            then(self.gen_receive_node('receive')).
-            then(self.gen_receive_list_node('rl')).
-            then(self.gen_receive_list_node('receive_list')).
-            then(self.gen_cancel_node('c')).
-            then(self.gen_cancel_node('cancel')).
-            then(self.gen_list_node('ls')).
-            then(self.gen_list_node('list')).
-            then(self.gen_player_node('player')).
-            then(self.gen_save_node('save')).
-            then(self.gen_reload_node('reload'))
+            # 下面的一行就是一条命令，多个 then 意味着别名/缩写
+            then(self.gen_post_node('p')).then(self.gen_post_node('post')).
+            then(self.gen_post_list_node('pl')).then(self.gen_post_list_node('post_list')).
+            then(self.gen_receive_node('r')).then(self.gen_receive_node('receive')).
+            then(self.gen_receive_list_node('rl')).then(self.gen_receive_list_node('receive_list')).
+            then(self.gen_cancel_node('c')).then(self.gen_cancel_node('cancel')).
+            then(self.gen_list_node('ls')).then(self.gen_list_node('list')).
+            then(self.gen_player_node('player'))#.
+            # 响应还没有做好，暂时关闭这两条命令
+            # then(self.gen_save_node('save')).
+            # then(self.gen_reload_node('reload'))
         )
 
 
---
+++ src/MCDRpost/mcdrpost/manager/version_manager.py
@@ -0,0 +1,84 @@
+"""版本管理器，根据 Minecraft 版本提供相应的功能实现"""
+
+from typing import TYPE_CHECKING
+
+from mcdrpost.data_structure import Item
+from mcdrpost.environment import Environment
+from mcdrpost.version import after17before20_5, after20_5, before17
+
+if TYPE_CHECKING:
+    from new.manager.post_manager import PostManager  # noqa
+
+
+class VersionManager:
+    """版本管理器，用于处理不同Minecraft版本的特性差异
+
+    根据服务器的 Minecraft 版本，为不同版本提供相应的功能实现，包括物品替换、
+    字典转物品对象、物品对象转字符串等功能。
+
+    Attributes:
+        environment (Environment): 环境信息对象，包含服务器版本等信息
+    """
+
+    def __init__(self, pm: "PostManager"):
+        """初始化版本管理器
+
+        Args:
+            pm (PostManager): PostManager 实例
+        """
+        self._post_manager = pm
+        self.environment: Environment = Environment(pm.server)
+        self._replace = None
+        self._dict2item = None
+        self._item2str = None
+
+    def refresh(self) -> None:
+        """刷新版本相关函数引用
+
+        根据当前服务器版本，更新内部函数引用
+        """
+        if self.environment.server_version < "1.17":
+            self._replace = before17.replace
+            self._dict2item = before17.dict2item
+            self._item2str = before17.item2str
+        elif self.environment.server_version < "1.20.5":
+            self._replace = after17before20_5.replace
+            self._dict2item = after17before20_5.dict2item
+            self._item2str = after17before20_5.item2str
+        else:
+            self._replace = after20_5.replace
+            self._dict2item = after20_5.dict2item
+            self._item2str = after20_5.item2str
+
+    # 下面是是依赖版本的函数
+
+    def replace(self, player: str, item: str) -> None:
+        """替换玩家副手物品
+
+        Args:
+            player (str): 玩家名
+            item (str): 物品字符串
+        """
+        self._replace(self._post_manager.server, player, item)
+
+    def dict2item(self, item: dict) -> Item:
+        """将字典转换为物品对象
+
+        Args:
+            item (dict): 物品字典
+
+        Returns:
+            Item: 物品对象
+        """
+        return self._dict2item(item)
+
+    def item2str(self, item: Item) -> str:
+        """将物品对象转换为命令字符串
+
+        Args:
+            item (Item): 物品对象
+
+        Returns:
+            str: 物品命令字符串
+        """
+        return self._item2str(item)
