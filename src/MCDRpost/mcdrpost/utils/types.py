8d6ec0e80d467b8834c71c2595e64db12a9b40eb
xieyuen <xieyuenol@outlook.com>
2025-08-27 18:49:17 +0800

    重构version

--- src/MCDRpost/mcdrpost/utils/types.py
+++ src/MCDRpost/mcdrpost/utils/types.py
@@ -2,7 +2,16 @@ from typing import Protocol
 
 from mcdreforged import PluginServerInterface
 
+from mcdrpost.data_structure import Item
+
 
 class ReplaceFunction(Protocol):
-    def __call__(self, server: PluginServerInterface, player: str, item: str) -> None: ...
+    def __call__(self, server: PluginServerInterface, player: str, item: Item) -> None: ...
 
+
+class Item2StrFunction(Protocol):
+    def __call__(self, item: Item) -> str: ...
+
+
+class Dict2ItemFunction(Protocol):
+    def __call__(self, item: dict) -> Item: ...
