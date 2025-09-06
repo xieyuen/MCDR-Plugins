55c3b43dc7b7b5206fdd271a1f993452cddbd99b
xieyuen <xieyuenol@outlook.com>
2025-09-06 10:16:21 +0800

    Feature/refactor version compatible (#10)
    * update
    
    * update
    
    * 优化导入方式方便新版本开发
    
    * fix errors
    
    * pre-release beta2
    
    * update
    
    * update readme
    
    and add migration work to todos
    
    * Update README.md

--- src/MCDRpost/mcdrpost/environment.py
+++ src/MCDRpost/mcdrpost/environment.py
@@ -7,7 +7,13 @@ class Environment:
 
     @property
     def server_version(self) -> str | None:
+        """Minecraft 服务器版本"""
         return self._server.get_server_information().version
+
+    @property
+    def mcdr_handler(self) -> str:
+        """MCDR 正在使用的 handler"""
+        return self._server.get_mcdr_config()['handler']
 
 
 __all__ = ['Environment']
---
+++ src/MCDRpost/mcdrpost/version_handler/abstract_version_handler.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+from mcdreforged import PluginServerInterface
+
+from mcdrpost.data_structure import Item
+
+
+class AbstractVersionHandler(ABC):
+    """版本处理器
+
+    Attributes:
+        server (PluginServerInterface): psi 实例
+    """
+
+    def __init__(self, server: PluginServerInterface):
+        self.server = server
+
+    @abstractmethod
+    def replace(self, player: str, item: str) -> None:
+        """替换玩家副手物品
+
+        Args:
+            player (str): 玩家名
+            item (str): 物品字符串
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def dict2item(item: dict) -> Item:
+        """将物品字典转换为物品对象
+
+        Args:
+            item (dict): 物品字典
+
+        Returns:
+            Item: 物品对象
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def item2str(item: Item) -> str:
+        """将物品对象转换为物品字符串
+
+        Args:
+            item (Item): 物品对象
+
+        Returns:
+            str: 物品字符串
+        """
+        raise NotImplementedError
