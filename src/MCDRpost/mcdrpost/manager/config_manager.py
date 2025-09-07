77d456787bc6e3a6dc6f046470590779bd60e74e
xieyuen <xieyuenol@outlook.com>
2025-09-07 13:49:33 +0800

    update
    see CHANGELOG.md

--- src/MCDRpost/mcdrpost/manager/config_manager.py
+++ src/MCDRpost/mcdrpost/manager/config_manager.py
@@ -11,39 +11,24 @@ if TYPE_CHECKING:
 
 
 class ConfigurationManager:
-    """配置管理器
-
-    Attributes:
-        configuration (Configuration): 插件配置
-    """
+    """配置管理器"""
 
     def __init__(self, post_manager: "PostManager") -> None:
         self._post_manager: "PostManager" = post_manager
         self._server: PluginServerInterface = post_manager.server
-        self.configuration: Configuration = self._server.load_config_simple(
-            constants.CONFIG_FILE_NAME,
-            target_class=Configuration,
-            file_format=constants.CONFIG_FILE_TYPE,
-        )
+        self._configuration: Configuration | None = None
 
     def reload(self) -> None:
+        """(重新)加载配置文件"""
         self._server.logger.info(tr(Tags.config.load))
-        self.configuration = self._server.load_config_simple(
+        self._configuration = self._server.load_config_simple(
             constants.CONFIG_FILE_NAME,
             target_class=Configuration,
             file_format=constants.CONFIG_FILE_TYPE,
-        )
-
-    def save(self) -> None:
-        self._server.logger.info(tr(Tags.config.save))
-        self._server.save_config_simple(
-            self.configuration,
-            constants.CONFIG_FILE_NAME,
-            file_format=constants.CONFIG_FILE_TYPE
         )
 
     def get_configuration(self) -> Configuration:
-        return self.configuration
+        return self._configuration
 
 
 __all__ = ['ConfigurationManager']
