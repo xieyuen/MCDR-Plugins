1a9f5a35626bc3e4542a5c2eced4bee618fdfead
xieyuen <xieyuenol@outlook.com>
2025-09-20 20:06:42 +0800

    优化SemanticVersion.patch的实现方式

--- src/MCDRpost/mcdrpost/utils/version.py
+++ src/MCDRpost/mcdrpost/utils/version.py
@@ -1,4 +1,3 @@
-import re
 from functools import total_ordering
 
 from packaging import version
@@ -10,17 +9,7 @@ class SemanticVersion:
         self.version = version.parse(version_string)
         self.major = self.version.major
         self.minor = self.version.minor
-
-        # 定义patch版本号，优先使用micro属性，否则从release元组中获取
-        if hasattr(self.version, 'micro'):
-            self.patch = self.version.micro
-        else:
-            # 从release元组中获取patch版本，如果不存在则默认为0
-            if len(self.version.release) > 2:
-                self.patch = self.version.release[2]
-            else:
-                self.patch = 0
-
+        self.patch = self.version.micro
         self.pre_release = self.version.pre
         self.build_metadata = self.version.local
 
