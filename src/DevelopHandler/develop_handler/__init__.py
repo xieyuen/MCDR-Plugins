5e19f60903d5a9824486ecc2ef52744a17a1ec54
xieyuen <xieyuenol@outlook.com>
2025-09-23 14:47:23 +0800

    DevelopHandler v0.1.2 (#15)
    * 0.1.0
    
    * 0.1.1: test
    
    * 0.1.2
    
    * v0.1.2
    
    * 0.1.1: test
    
    * 0.1.2

--- src/DevelopHandler/develop_handler/__init__.py
+++ src/DevelopHandler/develop_handler/__init__.py
@@ -1,9 +1,7 @@
 import re
 
-from mcdreforged import Info, Literal, PluginServerInterface, Serializable, new_thread
+from mcdreforged import Info, PluginServerInterface
 from mcdreforged.handler.impl import VanillaHandler
-
-import minecraft_data_api as api
 
 
 class DevelopHandler(VanillaHandler):
@@ -11,55 +9,13 @@ class DevelopHandler(VanillaHandler):
         return "develop_handler"
 
     def parse_server_stdout(self, text: str) -> Info:
+        # 简单粗暴
+        # 但是会导致终端和文件不相同
+        # 待优化
         text = re.sub(r'\[Not Secure] \[([^]]+)]', r'<\1>', text)
 
         return super().parse_server_stdout(text)
 
 
-class Configuration(Serializable):
-    test: bool = True
-    test_player: str = 'Test'
-
-
-config: Configuration
-player_list: list[str]
-
-
-def test(server: PluginServerInterface):
-    server.register_command(
-        Literal('!!test')
-        .runs(lambda: server.execute(f'execute as {config.test_player} run say !!test2'))
-    )
-    server.register_command(
-        Literal('!!test2')
-        .runs(lambda src, ctx: server.logger.info(f'Event.test({src=}, {ctx=})'))
-    )
-
-
 def on_load(server: PluginServerInterface, _old):
-    global config
     server.register_server_handler(DevelopHandler())
-
-    config = server.load_config_simple(target_class=Configuration)
-
-    if config.test:
-        test(server)
-
-    if server.is_server_running():
-        on_server_startup(server)
-
-
-@new_thread("DevelopHandler | Test")
-def get_online_player():
-    global player_list
-    player_list = api.get_server_player_list().players
-
-
-def on_server_startup(server: PluginServerInterface):
-    get_online_player().join()
-
-    if config.test_player not in player_list:
-        server.execute(f'player {config.test_player} spawn')
-
-    if config.test:
-        server.execute('say !!test')
