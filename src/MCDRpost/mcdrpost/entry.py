9e1db2dbd4b2160546cc1cde27a061a39075b44c
xieyuen <xieyuenol@outlook.com>
2025-10-05 12:35:10 +0800

    初步拆分PostManager

--- src/MCDRpost/mcdrpost/entry.py
+++ src/MCDRpost/mcdrpost/entry.py
@@ -8,26 +8,27 @@ from mcdreforged import PluginServerInterface
 from mcdrpost.manager.post_manager import PostManager
 from mcdrpost.version_handler import register_all_handlers
 
-manager: PostManager = PostManager(PluginServerInterface.psi())
+pm: PostManager = PostManager(PluginServerInterface.psi())
+event_emitter = pm.event_manager
 
 register_all_handlers()
 
 
 def on_load(server: PluginServerInterface, prev_module):
-    manager.on_load(server, prev_module)
+    event_emitter.on_load(server, prev_module)
 
 
 def on_unload(server: PluginServerInterface):
-    manager.on_unload(server)
+    event_emitter.on_unload(server)
 
 
 def on_server_startup(server: PluginServerInterface):
-    manager.on_server_startup(server)
+    event_emitter.on_server_startup(server)
 
 
 def on_server_stop(server: PluginServerInterface, server_return_code: int):
-    manager.on_server_stop(server, server_return_code)
+    event_emitter.on_server_stop(server, server_return_code)
 
 
 def on_player_joined(server, player, info):
-    manager.on_player_joined(server, player, info)
+    event_emitter.on_player_joined(server, player, info)
--- src/MCDRpost/mcdrpost/manager/post_manager.py
+++ src/MCDRpost/mcdrpost/manager/post_manager.py
@@ -1,15 +1,14 @@
-import time
 from typing import Literal
 
-from mcdreforged import Info, InfoCommandSource, PluginServerInterface, new_thread
+from mcdreforged import InfoCommandSource, PluginServerInterface
 
-import minecraft_data_api as api
 from mcdrpost import constants
 from mcdrpost.configuration import Configuration
 from mcdrpost.data_structure import Item, OrderInfo
 from mcdrpost.manager.command_manager import CommandManager
 from mcdrpost.manager.config_manager import ConfigurationManager
 from mcdrpost.manager.data_manager import DataManager
+from mcdrpost.manager.event_manager import EventManager
 from mcdrpost.manager.version_manager import VersionManager
 from mcdrpost.utils import get_formatted_time
 from mcdrpost.utils.exception import InvalidItem
@@ -17,7 +16,7 @@ from mcdrpost.utils.translation import TranslationKeys
 
 
 class PostManager:
-    """Post 管理器，也是插件核心逻辑处理的地方
+    """插件核心功能处理，兼协同各模块
 
     Attributes:
         server (PluginServerInterface): MCDR插件接口
@@ -32,67 +31,11 @@ class PostManager:
         self.data_manager: DataManager = DataManager(self)
         self.command_manager: CommandManager = CommandManager(self)
         self.version_manager: VersionManager = VersionManager(self.server)
+        self.event_manager: EventManager = EventManager(self)
 
     @property
     def config(self) -> Configuration:
         return self.config_manager.get_config()
-
-    # Events Handle
-    def on_load(self, server: PluginServerInterface, _prev_module) -> None:
-        """事件: 插件加载--在这里会注册插件的命令
-
-        如果服务端已经运行（也就是重新加载插件的情况），会自动地引发服务端启动完成事件
-
-        .. note::
-            PostManager在插件导入时通过 ``PluginServerInterface.psi()`` 获取到
-                PluginServerInterface 实例进行实例化，
-                而非一般的在 on_load() 内得到 PluginServerInterface 实例再实例化
-        """
-        self.config_manager.reload()
-        self.data_manager.reload()
-        self.command_manager.register()
-        if server.is_server_running():
-            self.on_server_startup(server)
-
-    def on_unload(self, _server: PluginServerInterface) -> None:
-        """事件: 插件卸载--保存订单信息"""
-        self.data_manager.save()
-
-    def on_player_joined(self, server: PluginServerInterface, player: str, _info: Info) -> None:
-        """事件: 玩家加入服务器"""
-        if not self.data_manager.is_player_registered(player):
-            if self.config.auto_register:
-                # 还未注册的玩家
-                self.data_manager.add_player(player)
-                server.logger.info(TranslationKeys.login_log.tr(player))
-                self.data_manager.save()
-                return
-
-            # 通知权限在admin以上的管理员有新玩家加入
-            player_list = api.get_server_player_list()[-1]
-            for online_player in player_list:
-                if server.get_permission_level(online_player) >= 3:
-                    server.tell(online_player, TranslationKeys.new_player_joined.tr(player))
-            server.logger.info(TranslationKeys.new_player_joined.tr(player))
-            return
-
-        # 已注册的玩家，向他推送订单消息（如果有）
-        if self.data_manager.has_unreceived_order(player):
-            @new_thread('MCDRpost | send receive tip')
-            def send_receive_tip():
-                time.sleep(self.config.receive_tip_delay)
-                server.tell(player, TranslationKeys.wait_for_receive.tr())
-                self.version_manager.play_sound.has_something_to_receive(player)
-
-            send_receive_tip()
-
-    def on_server_startup(self, _server: PluginServerInterface):
-        """事件: 服务器完全开启--刷新游戏版本设置"""
-        self.version_manager.refresh()
-
-    def on_server_stop(self, _server: PluginServerInterface, _server_return_code: int):
-        """事件: 服务器关闭--保存配置信息和订单信息"""
-        self.data_manager.save()
 
     # Helper methods
     def replace(self, player: str, item: Item) -> None:
