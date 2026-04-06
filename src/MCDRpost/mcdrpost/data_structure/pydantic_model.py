90c281f90b737120721c9b58abd6a0d9d2ea3ac6
xieyuen <xieyuenol@outlook.com>
2026-04-06 14:10:04 +0800

    docs(data_structure): 更新版本号文档
    - 将版本号从 3.4.1 更新为 4.0
    - 修正了 components 描述中的空格问题
    - 统一了多个文件中的版本号标记

--- src/MCDRpost/mcdrpost/data_structure/pydantic_model.py
+++ src/MCDRpost/mcdrpost/data_structure/pydantic_model.py
@@ -7,9 +7,9 @@ class Item(BaseModel):
     """物品数据类，表示 Minecraft 中的物品
 
     .. note::
-        Minecraft 1.20.5 之后的 components 数据和之前的 tag 标签都存放在 components中
+        Minecraft 1.20.5 之后的 components 数据和之前的 tag 标签都存放在 components 中
 
-    .. versionadded:: 3.4.1
+    .. versionadded:: 4.0.0
         新增验证功能
     """
 
